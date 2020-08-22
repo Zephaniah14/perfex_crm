d6d05b1e120b3f442037a886f67ae36f6478191b
Zephaniah Langley <millennialbusinessbuilders@gmail.com>
2020-08-22 16:54:21 -0400

    changed from dev to prod mode

--- application/logs/log-2020-08-22.php
+++ application/logs/log-2020-08-22.php
@@ -3,3 +3,5 @@
 ERROR - 2020-08-22 22:05:41 --> Query error: Table 'heroku_97cd44a4ee7c714.tblsessions' doesn't exist - Invalid query: SELECT 1
 FROM `tblsessions`
 WHERE `id` = 'bdve4h21ol3tti4eu3bd8h7r8cgv0o2g'
+ERROR - 2020-08-22 22:12:13 --> Severity: Notice --> Trying to get property 'version' of non-object C:\xampp\htdocs\perfex_crm\application\libraries\App.php 110
+ERROR - 2020-08-22 22:12:14 --> Severity: Notice --> Trying to get property 'version' of non-object C:\xampp\htdocs\perfex_crm\application\libraries\App.php 110
