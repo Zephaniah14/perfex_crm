ae46ac2afd1476fac6fe29bce7a098e033e07207
Zephaniah Langley <millennialbusinessbuilders@gmail.com>
2020-08-22 16:06:29 -0400

    enabled dev mode

---
+++ application/logs/log-2020-08-22.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-08-22 22:05:41 --> Query error: Table 'heroku_97cd44a4ee7c714.tblsessions' doesn't exist - Invalid query: SELECT 1
+FROM `tblsessions`
+WHERE `id` = 'bdve4h21ol3tti4eu3bd8h7r8cgv0o2g'
